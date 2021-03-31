2eff08a66c6b659d0cb0d17f44bb8e10641ad61e
Richcholo <66872319+Richcholo@users.noreply.github.com>
2021-03-31 12:10:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-document.getElementById("button").addEventListener("click", function() {
+document.body.style.backgroundColor = "#000000";
+document.body.style.backgroundRepeat = "no-repeat";
+document.getElementById("buttontest").addEventListener("click", function() {
   // Change background to dancing rick
-    document.body.style.backgroundImage = "url('rickroll.gif')"
+  document.body.style.backgroundImage = "url('rickroll.gif')"
   // Hide the heading and the button
   document.querySelector(".troll").style.display = "none"
 
   // Play song
   var song = document.getElementById("song")
   song.play()
+
+  // Change background size
+  document.body.style.backgroundSize = "100% 100%"
 })
